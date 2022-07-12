16e0961f997859d898c2c87736a62357ed2cce67
jbep <joshuabemartin@gmail.com>
2022-07-12 14:33:47 -0400

    putting in my resources on learning how to do this

--- js/Overworld.js
+++ js/Overworld.js
@@ -1,3 +1,7 @@
+// explanation on sprite sheet https://www.codeandweb.com/what-is-a-sprite-sheet,
+// how to create a sprite sheet https://www.codeandweb.com/texturepacker/tutorials/how-to-create-a-sprite-sheet,
+// https://itch.io/game-assets/tag-top-down is where I am finding assets
+
 // element is the most general base class from which all element objects in a document inherit
 // element inherits properties from its parent interface
 
