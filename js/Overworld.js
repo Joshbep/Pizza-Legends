d11e80515e8e8e7f48e08b9c2da4cdcbb2966425
jbep <joshuabemartin@gmail.com>
2022-07-12 22:29:01 -0400

    added update for every frame

--- js/Overworld.js
+++ js/Overworld.js
@@ -1,37 +1,41 @@
+// https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/drawImage
 // explanation on sprite sheet https://www.codeandweb.com/what-is-a-sprite-sheet,
 // how to create a sprite sheet https://www.codeandweb.com/texturepacker/tutorials/how-to-create-a-sprite-sheet,
 // https://itch.io/game-assets/tag-top-down is where I am finding assets
-
 // element is the most general base class from which all element objects in a document inherit
 // element inherits properties from its parent interface
 
-class Overworld {
-  constructor(config) {
-    this.element = config.element;
-    this.canvas = this.element.querySelector('.game-canvas');
-    this.ctx = this.canvas.getContext('2d');
-  }
-  init() {
-    const image = new Image ();
-    image.onload = () => {
-      this.ctx.drawImage(image,0,0)
-    };
-    image.src = "./images/maps/DemoLower.png";
-
-    //Place some Game Objects
-    const hero = new GameObject({
-      x: 5,
-      y: 6,
-    })
-    const npc1 = new GameObject({
-      x: 7,
-      y: 9,
-      src: './images/characters/people/bad2.png'
-    })
-    setTimeout(() => {
-      hero.sprite.draw(this.ctx);
-      npc1.sprite.draw(this.ctx);
-    }, 200)
-  }
-
+ class Overworld {
+ constructor(config) {
+   this.element = config.element;
+   this.canvas = this.element.querySelector(".game-canvas");
+   this.ctx = this.canvas.getContext("2d");
+   this.map = null;
  }
+ startGameLoop() {
+   const step = () => {
+    //Clear off the canvas
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    //Draw Lower layer
+    this.map.drawLowerImage(this.ctx);
+    // Draw Game Objects
+    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_objects/Object/values
+    Object.values(this.map.gameObjects).forEach(object => {
+      object.x += 0.02;
+      object.sprite.draw(this.ctx);
+    })
+    //Draw Upper layer
+    this.map.drawUpperImage(this.ctx);
+    //https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
+    // this method is basically used when your ready to update your animation on screen
+     requestAnimationFrame(() => {
+      step();
+     })
+   }
+   step();
+ }
+ init() {
+  this.map = new OverWorldMap(window.OverworldMaps.Kitchen);
+  this.startGameLoop();
+ }
+}
